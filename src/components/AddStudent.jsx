5d4acdc9280c3008c1e49cb08954ab251cd395d7
akashpa12 <akash.pa.anandan00@gmail.com>
2024-06-11 14:55:37 +0530

    messege

--- src/components/AddStudent.jsx
+++ src/components/AddStudent.jsx
@@ -1,60 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
 
 const AddStudent = () => {
-  return (
-    <div>
-        <Navbar/>
-        <div className="container">
-            <div className="row">
-                <div className="col col-12 col-sm-12 md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <div className="row g-3">
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Name</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Admission No</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Rollno</label>
-                            <input type="text" className="form-control" />
+    const [data, setData] = useState(
+        {
+            "_id": "",
+            "fname": "",
+            "lname": "",
+            "college": "",
+            "dob": "",
+            "course": "",
+            "mobile": "",
+            "email": "",
+            "address": "",
+            "__v": ""
+        }
+    )
+    const inputHANDLER=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
 
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Parent Name</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <label htmlFor="" className="form-label">Collage Name</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Date of Birth</label>
-                            <input type="date" name="" id="" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Email id</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Password</label>
-                            <input type="password" name="" id="" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">conform password</label>
-                            <input type="password" name="" id="" className="form-control" />
+    }
+    const readVALUE=()=>
+        {
+            console.log(data)
+            axios.post("http://localhost:8080/add",data).then(
+                (response)=>{
+                    console.log(response.data)
+                        if (response.data.status=="success")
+                             {
+                            alert("success")
+                        } 
+                        else {
+                            alert("error")
+                        }
+                    
+                }
+            ).catch().finally()
+        }
+
+    return (
+        <div>
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">id</label>
+                                <input type="text" className="form-control" name='_id' value={data._id} onChange={inputHANDLER}/>
                             </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-success">Register</button>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Fname</label>
+                                <input type="text" className="form-control" name='fname' value={data.fname} onChange={inputHANDLER}/>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Lname</label>
+                                <input type="text" className="form-control" name="lname" value={data.lname} onChange={inputHANDLER} />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">College</label>
+                                <input type="text" className="form-control" name='college' value={data.college} onChange={inputHANDLER}/>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">DOB</label>
+                                <input type="text" className="form-control" name='dob' value={data.dob} onChange={inputHANDLER} />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Course</label>
+                                <input type="text" className="form-control"  name='course' value={data.course} onChange={inputHANDLER}/>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Mobile</label>
+                                <input type="text" className="form-control" name='mobile' value={data.mobile} onChange={inputHANDLER}/>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Email</label>
+                                <input type="text" className="form-control" name='email' value={data.email} onChange={inputHANDLER}/>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Address</label>
+                                <input type="text" className="form-control" name='address' value={data.address} onChange={inputHANDLER}/>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">__v</label>
+                                <input type="text" className="form-control" name='__v' value={data.__v} onChange={inputHANDLER}/>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <button className="btn btn-success" onClick={readVALUE}>Register</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default AddStudent
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,46 +1,71 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
 
 const ViewAll = () => {
-    const [Data,changeData]=useState(
-        [
-            {"name":"Aju","rollno":1,"Admno":101},
-            {"name":"Alen","rollno":2,"Admno":102},
-            {"name":"joe","rollno":5,"Admno":105},
-            {"name":"varun","rollno":7,"Admno":107},
-            {"name":"Aarun","rollno":4,"Admno":104},
-            {"name":"Abi","rollno":3,"Admno":103},
-            {"name":"Amrutha","rollno":8,"Admno":108},
-            {"name":"Eva","rollno":6,"Admno":106},
-        ]
-    )
+    const[data,changedata]=useState([])
+    const fetchData=()=>{
+        axios.get("http://localhost:8080/view").then(
+            (response)=>{
+                changedata(response.data)
+            }
+        ).catch(
+            (error)=>{
+                console.log(error.messege)
+                alert("error")
+            }
+        ).finally()
+
+    }
+    useEffect(()=>{fetchData()},[])
     return (
         <div>
-            <Navbar />
+            <Navbar/>
             <div className="container">
-                <div className="row.g-3">
+                <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <div className="row g-3">
-                            {Data.map(
-                                (value,index)=>{
-                                    return  <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="row">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                            <th scope="col">_id</th>
+                                            <th scope="col">firstname</th>
+                                            <th scope="col">lastname</th>
+                                            <th scope="col">college</th>
+                                            <th scope="col">dob</th>
+                                            <th scope="col">course</th>
+                                            <th scope="col">mobile</th>
+                                            <th scope="col">email</th>
+                                            <th scope="col">address</th>
+                                            <th scope="col">__v</th>
+                                        </tr>
+                                    </thead>
 
-
-
-                                    <div class="card">
-                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLv9ag4S0tjqq7dflHFUtEqskKt_81b2xlyQ&s" class="card-img-top" alt="..."></img>
-                                        <div class="card-body">
-                                            <h5 class="card-title">{value.name}</h5>
-                                            <p class="card-text">Rollno:{value.rollno}</p>
-                                            <p class="card-text">AdmissionNo:{value.Admno}</p>
-                                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                }
-                            )
-                            }
-                           
+                                        {data.map(
+                                            (value,index)=>{
+                                            return <tbody>
+                                        <tr>
+                                            <th scope="row">{value._id}</th>
+                                            <td>{value.fname}</td>
+                                            <td>{value.lname}</td>
+                                            <td>{value.college}</td>
+                                            <td>{value.dob}</td>
+                                            <td>{value.course}</td>
+                                            <td>{value.mobile}</td>
+                                            <td>{value.email}</td>
+                                            <td>{value.address}</td>
+                                            <td>{value.__v}</td>
+                                        </tr>
+                                        
+                                        
+                                    </tbody>
+                                            }
+                                        )}
+                                        
+                                    
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
